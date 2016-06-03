84fb02845e7e80c527f51922b8e4091addacd75d
fateme <f.sadeghi2012@gmail.com>
2016-06-03 10:30:23 +0430

    new class

--- OOD/src/ResourceManagment/HumanResource.java
+++
@@ -1,9 +0,0 @@
-package ResourceManagment;
-
-public class HumanResource extends Resource {
-
-	public HumanResource() {
-		// TODO Auto-generated constructor stub
-		super(ResourceType.HUMAN);
-	}
-}
