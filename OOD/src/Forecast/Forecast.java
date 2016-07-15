21a126861ca1e6c41250732af1fbfbaf436e41f4
ygnGhayour <yeganeghayour@gmail.com>
2016-07-15 22:46:40 +0330

    تخمین شروع شد

--- OOD/src/Forecast/Forecast.java
+++ OOD/src/Forecast/Forecast.java
@@ -6,7 +6,8 @@ import ResourceManagment.Resource;
 
 public class Forecast {
 
-	int size;
+	int humanCount;
+	int moduleCount;
 	ArrayList<Resource> guestResources;
 	ArrayList<String> technology;
 	
@@ -14,13 +15,20 @@ public class Forecast {
 	ArrayList<Resource> essentialResources;
 	ArrayList<Resource> nonessentialResources;
 	
-	public Forecast(int size,ArrayList<String> tech){
-		this.size=size;
+	public Forecast(int hum,int mod,ArrayList<String> tech){
+		this.humanCount=hum;
+		this.moduleCount=mod;
 		this.technology=tech;
 	}
 	
 	public Forecast(ArrayList<Resource> neededResource){
 		this.neededResource=neededResource;
+	}
+
+	public void estimate() {
+		// TODO Auto-generated method stub
+		//db.se
+		
 	}
 	
 	
--- OOD/src/UI/Estimate.java
+++ OOD/src/UI/Estimate.java
@@ -9,8 +9,14 @@ import java.awt.CardLayout;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
+
+import Forecast.Forecast;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 
 public class Estimate {
@@ -18,7 +24,9 @@ public class Estimate {
 	private JFrame frame;
 	private JTextField textField;
 	public int count=0;
-	public JTextField tfield;
+	public ArrayList<String> arrayTfield=new ArrayList<String>();
+	public static int ii=153;
+	
 
 	/**
 	 * Launch the application.
@@ -44,6 +52,15 @@ public class Estimate {
 		initialize();
 		
 	}
+	protected void addMore(JPanel panel,int i, int j , int k, int l ) {
+		// TODO Auto-generated method stub
+		 JTextField newtext = new JTextField();
+		 newtext.setBounds(i, j, k, l);
+         newtext.setName("tx" + count );
+         count++;
+         arrayTfield.add(newtext.getText());
+         panel.add(newtext);
+	}
 
 	/**
 	 * Initialize the contents of the frame.
@@ -62,42 +79,68 @@ public class Estimate {
 		label.setBounds(226, 46, 157, 14);
 		panel.add(label);
 		
-		JSpinner spinner = new JSpinner();
-		spinner.setBounds(173, 43, 29, 20);
-		panel.add(spinner);
+		JSpinner spinnerHum = new JSpinner();
+		spinnerHum.setBounds(209, 71, 29, 20);
+		panel.add(spinnerHum);
 		
 		JLabel label_1 = new JLabel("\u062A\u06A9\u0646\u0648\u0644\u0648\u0698\u06CC \u0647\u0627\u06CC \u0645\u0646\u062A\u062E\u0628:");
-		label_1.setBounds(265, 86, 102, 14);
+		label_1.setBounds(266, 156, 102, 14);
 		panel.add(label_1);
 		
 		textField = new JTextField();
-		textField.setBounds(154, 83, 101, 20);
+		textField.setBounds(155, 153, 101, 20);
 		panel.add(textField);
 		textField.setColumns(10);
+		//arrayTfield.add()
 		
 		
 		JButton button = new JButton("\u0627\u0636\u0627\u0641\u0647 \u06A9\u0631\u062F\u0646");
 		button.addActionListener(new ActionListener() {
 			
-
+			
 			public void actionPerformed(ActionEvent arg0) {
-				
-				    tfield = new JTextField();
-	                tfield.setName("technology" + count);
-	                count++;
-	                frame.add(tfield);
-	                frame.revalidate();  // For JDK 1.7 or above.
-	                //frame.getContentPane().revalidate(); // For JDK 1.6 or below.
-	                frame.repaint();
+				 addMore(panel,155,ii+25,101,20);
 			}
 		});
-		button.setBounds(55, 82, 89, 23);
+		button.setBounds(48, 152, 89, 23);
 		panel.add(button);
+		
+		JLabel lblNewLabel = new JLabel("\u0633\u0627\u06CC\u0632 \u0627\u0646\u0633\u0627\u0646\u06CC:");
+		lblNewLabel.setBounds(266, 71, 68, 14);
+		panel.add(lblNewLabel);
+		
+		JLabel label_2 = new JLabel("\u0633\u0627\u06CC\u0632 \u062A\u06A9\u0646\u06CC\u06A9\u06CC:");
+		label_2.setBounds(266, 110, 77, 14);
+		panel.add(label_2);
+		
+		JLabel label_3 = new JLabel("\u0646\u0641\u0631");
+		label_3.setBounds(155, 74, 46, 14);
+		panel.add(label_3);
+		
+		JSpinner spinnerModule = new JSpinner();
+		spinnerModule.setBounds(209, 107, 29, 20);
+		panel.add(spinnerModule);
+		
+		JLabel label_4 = new JLabel("\u0645\u0627\u0698\u0648\u0644");
+		label_4.setBounds(153, 110, 46, 14);
+		panel.add(label_4);
+		
+		JButton button_1 = new JButton("\u062A\u062E\u0645\u06CC\u0646 \u0645\u0646\u0627\u0628\u0639");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int peopleNum= (Integer) spinnerHum.getValue();			
+				int moduleNum= (Integer) spinnerModule.getValue();
+				Forecast fc=new Forecast(peopleNum,moduleNum,arrayTfield);
+				fc.estimate();
+				//textField
+			}
+		});
+		button_1.setBounds(48, 211, 89, 23);
+		panel.add(button_1);
 		
 		
 		
 		JPanel panel_1 = new JPanel();
 		frame.getContentPane().add(panel_1, "name_86830926283181");
 	}
-
 }
